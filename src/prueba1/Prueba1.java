47074705b8e8f3dcd5bc8c7f051468f36bb4c996
andu0421 <andu112103@gmail.com>
2022-11-09 16:20:39 -0500

    Cambio 1

---
+++ src/prueba1/Prueba1.java
@@ -0,0 +1,23 @@
+
+package prueba1;
+
+public class Prueba1 {
+
+  
+    public static void main(String[] args) {
+        
+        int n1;
+        int n2;
+        int suma;
+        
+        n1 = 20;
+        n2 = 15;
+        suma = n1 + n2;
+        
+        System.out.println(suma);
+        
+        System.out.println("Hola mundo");
+        
+    }
+    
+}
