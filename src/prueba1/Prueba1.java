84e8d7e45c3bd62172e120f54e5985a53e4b1b87
andu0421 <andu112103@gmail.com>
2022-11-12 09:09:55 -0500

    Calculadora

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -1,25 +1,38 @@
-
 package prueba1;
+
+import java.util.Scanner;
 
 public class Prueba1 {
 
-  
     public static void main(String[] args) {
-        
+
         int n1;
         int n2;
-        int suma;
+        int resultado = 0;
+
+        Scanner consola = new Scanner(System.in);
+        System.out.println("Digite un numero: ");
+        n1 = Integer.parseInt(consola.nextLine());
+        System.out.println("Digite otro numero: ");
+        n2 = Integer.parseInt(consola.nextLine());
+        System.out.println("Que Operacion deseas Realizar: ");
+        String respuesta = consola.nextLine();
         
-        n1 = 20;
-        n2 = 15;
-        suma = n1 + n2;
-        
-        System.out.println(suma);
-        
-        System.out.println("Hola mundo");
-        
-        System.out.println("Creo que ya jajaja");
-        
-    }
-    
+        if("+".equals(respuesta)){
+            resultado = (n1 + n2);
+            
+        }
+        else if("-".equals(respuesta)){
+            resultado = (n1 - n2);
+        }
+           else if("*".equals(respuesta)){
+            resultado = (n1 * n2);
+        }
+           else if("/".equals(respuesta)){
+            resultado = (n1 / n2);
+        }
+        System.out.println("respuesta = " + String.valueOf(resultado));
+
+    } 
+
 }
